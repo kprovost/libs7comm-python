b03fe580af7149b63d4d4a978d865670e02608f8
Kristof Provost <kristof@codepro.be>
2013-07-14 22:31:48 +0200

    Take liibs7comm versioning into account.
    Explicitly go for version 0.1 for now[1] although later we'll want to
    take major version 0.
    
    [1] Because the C lib doesn't properly install symlinks yet.

--- s7/__init__.py
+++ s7/__init__.py
@@ -4,7 +4,7 @@ import ctypes
 class S7Comm:
     def __init__(self, address):
         self._address = address
-        self._s7obj = ctypes.CDLL("libs7comm.so")  
+        self._s7obj = ctypes.CDLL("libs7comm.so.0.1")
 
         c_address = ctypes.create_string_buffer("10.0.3.9")
         self._s7conn = self._s7obj.s7comm_connect(c_address)
